07b1591ff94be57940a295254dec5a80aa09c81d
Jeronimo <jeronimotornini@gmail.com>
2023-03-29 18:21:12 -0300

    Implementacion de Navegacion

---
+++ Src/Navigation/Navigation.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../../Screens/Home"; //Home Screen
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
