f543c5db2e393c598e452c9c80df5474e0ca7fa2
Jeronimo <jeronimotornini@gmail.com>
2023-03-30 16:59:23 -0300

    Navegacion con pantallas

--- Src/Navigation/Navigation.js
+++ Src/Navigation/Navigation.js
@@ -1,7 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Octicons, MaterialIcons } from "@expo/vector-icons"; //Expo Icons
 import Home from "../../Screens/Home"; //Home Screen
+import Add from "../../Screens/Add"; //Add Screen
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -9,8 +11,32 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarActiveTintColor: "#00d7ff",
+          tabBarInactiveTintColor: "grey",
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => {
+              return <Octicons name="home" size={size} color={color} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Add"
+          component={Add}
+          options={{
+            tabBarIcon: ({ color, size }) => {
+              return <MaterialIcons name="add" size={size} color={color} />;
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
