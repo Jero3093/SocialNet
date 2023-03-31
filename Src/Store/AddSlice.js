05cb8738ee52a99cac71bbc0c907c88976497ff0
Jeronimo <jeronimotornini@gmail.com>
2023-03-31 15:46:59 -0300

    Slice de Agregar

--- Src/Store/AddSlice.js
+++ Src/Store/AddSlice.js
@@ -7,5 +7,18 @@ const initialState = {
 export const AddSlice = createSlice({
   name: "AddSlice",
   initialState,
-  reducers: {},
+  reducers: {
+    GetItems: (state, action) => {
+      const items = action.payload.Items; //Data from the Add Screen by Dispatch
+      const AddItems = state.Items.find((item) => item.Id === items.Id);
+
+      if (AddItems) {
+        state.Items = state.Items.filter((item) => item.Id !== items.Id);
+      } else {
+        state.Items.push(items); //Push all the Data to the Items State
+      }
+    },
+  },
 }); //Container of the function to change the States
+
+export const ItemsCount = (state) => state.AddSlice.Items.length; //Funtion to get the number of items in the State
